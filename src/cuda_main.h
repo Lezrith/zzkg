22261cb3dfc1196b71977f8142e6afc594b1a06e
Krzysztof Tomczak <krzysztof.g.tomczak@student.put.poznan.pl>
2019-05-21 23:01:52 +0200

    Add project

---
+++ src/cuda_main.h
@@ -0,0 +1,6 @@
+#ifndef ZZKG_CUDA_CUDA_MAIN_H
+#define ZZKG_CUDA_CUDA_MAIN_H
+
+int cuda_main(int argc, char **argv);
+
+#endif //ZZKG_CUDA_CUDA_MAIN_H
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "cuda_main.h"
+
+int main(int argc, char **argv) {
+    return cuda_main(argc, argv);
+}
